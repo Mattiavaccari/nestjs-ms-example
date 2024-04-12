13cfa3daa9f97ed8f094fe5e1183c7f19df5a2e4
Mattia vaccari <122625102+Mattiavaccari@users.noreply.github.com>
2024-04-12 16:15:59 +0000

    Edit Controller CU-86byavrrr

--- src/gateway/animals.controller.ts
+++ src/gateway/animals.controller.ts
@@ -71,4 +71,13 @@ export class AnimalsController {
     return AnimalRTO.fromAnimalRTO(response);
   }
 
+  @Post('/animal')
+  async create3Animal(@Body() body: CreateAnimalDTO): Promise<AnimalRTO> {
+    const pattern = `${body.animalType}.createOne`;
+    const response = await this.networking.send<DogRTO | CatRTO>(pattern, {
+      name: body.name,
+    });
+
+    return AnimalRTO.fromAnimalRTO(response);
+  }
 }
